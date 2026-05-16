8b4debb6827be9249a9242409614b883a7fb0a52
Nikhil-git <voyagerelev@gmail.com>
2026-05-16 21:05:07 +0530

    fixed login page syntax

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 export default function LoginPage() {
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+    <div>
       Login Page
     </div>
   );
